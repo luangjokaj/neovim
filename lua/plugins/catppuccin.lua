b19cb2c67beb74115594b0651a2167d02296fe4e
Luan Gjokaj <luan@riangle.com>
2024-03-25 22:06:58 +0100

    feat: code formatters

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,17 +1,17 @@
-return { 
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup {
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        }
-      }
-    }
-    vim.cmd.colorscheme "catppuccin"
-  end
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
