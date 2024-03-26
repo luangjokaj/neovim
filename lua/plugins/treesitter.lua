50617b2ad35a2ec4d91e635a81e4f8e7608ef691
Luan Gjokaj <luan@riangle.com>
2024-03-26 01:19:57 +0100

    feat: auto install treesitter and add toggleterm

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "html" },
+      auto_install = true,
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
