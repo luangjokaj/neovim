34ee7cc601a9cf5ced96de48b6fb4d84033ff77c
Luan Gjokaj <luan@riangle.com>
2024-03-27 00:06:36 +0100

    feat: update keystrokes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,8 +33,8 @@ return {
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
