1e7d3374f19d8f140867b0f014821c700759d9b0
Luan Gjokaj <luan@riangle.com>
2024-04-06 20:18:39 +0200

    feat: neo-tree keymap

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree position=left<CR>", {})
+		vim.keymap.set("n", "<leader>tre", ":Neotree position=left<CR>", {})
 		vim.keymap.set("n", "<leader>show", ":Neotree reveal<CR>", {})
 	end,
 }
