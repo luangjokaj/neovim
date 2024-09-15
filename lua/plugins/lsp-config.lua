42a83164f66fb3cc2016088038a6c03d52f3617c
Luan Gjokaj <luan@riangle.com>
2024-09-15 10:56:24 +0200

    chore: update plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "intelephense" },
+        ensure_installed = { "lua_ls", "ts_ls", "intelephense" },
       })
     end,
   },
