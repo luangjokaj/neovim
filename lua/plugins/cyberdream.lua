3d757402d61437847f910df63f746e1e4578d7df
Luan Gjokaj <luan@riangle.com>
2024-12-28 01:51:05 +0100

    feat: borderless telescope

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -8,7 +8,7 @@ return {
       transparent = true,
       italic_comments = true,
       hide_fillchars = true,
-      borderless_telescope = true,
+      borderless_telescope = false,
       terminal_colors = true,
     })
     vim.cmd("colorscheme cyberdream") -- set the colorscheme
