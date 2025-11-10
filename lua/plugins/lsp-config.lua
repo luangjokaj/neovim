b4cf35af72e2ee2a6c3c5cd63c3f48d38a3d8f1c
Luan Gjokaj <luan@riangle.com>
2025-11-10 10:55:44 +0100

    feat: cs key

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,7 +25,7 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>cs", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
